3b75e15479fdbd2294f2015043185d96166775c6
Your Name <you@example.com>
2026-03-19 19:14:13 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -72,7 +72,7 @@ posiciona en el elemento anterior.
 void eliminaElementos(List*L, int elem){
    int *elemento = first(L);
    while(elemento != NULL){
-      if((elemento == elem) popCurrent(L);
+      if(*(int*)elemento == elem) popCurrent(L);
       
       elemento = next(L);
    }
